1e59691874a28327647d0aeeefb90841e953d1d2
Siah <siah@goodwork.sg>
2023-10-25 18:29:47 +0800

    feat: instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,17 @@ export default function Home() {
   const r = useRouter();
   return (
     <div>
+      <div className="flex items-center">
+        Copy this:
+        <img
+          src="/deploy.png"
+          alt="deploy id"
+          className="w-[700px] h-[40px] block"
+        />
+      </div>
+      <hr className="my-4" />
       <div className="flex gap-2 items-center">
-        Check deploy status:{" "}
+        Paste here:
         <input
           type="text"
           name="deployId"
