96d559400b57d2fc4139797dd9f761d860a9ba16
Siah <siah@goodwork.sg>
2023-10-27 13:24:40 +0800

    feat: Try auto refresh

--- src/app/e/[id]/page.tsx
+++ src/app/e/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+export const revalidate = 0;
+
 export default function Page({ params }: { params: { id: string } }) {
+  const r = useRouter();
+
+  useEffect(() => {
+    window.setTimeout(() => {
+      r.refresh();
+    }, 2000);
+  }, []);
   const id = params.id;
+
   return (
     <div>
       <img
